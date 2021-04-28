c34468f0e930452f72c4de9d0a1ae28be40b2b8d
ttruty <ttruty@gmail.com>
2021-04-27 20:18:29 -0500

    filter results on discovery page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -3,6 +3,7 @@ import { PlacesService } from './../places.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-discover',
@@ -13,14 +14,19 @@ export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listLoadedPlaces: Place[];
   private placesSub: Subscription;
+  releventPlaces: Place[];
+  private filter = 'all';
 
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private authService: AuthService) { }
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listLoadedPlaces = this.loadedPlaces.slice(1);
+      this.segmentChanged(this.filter);
+      this.releventPlaces = this.loadedPlaces;
+      console.log(this.releventPlaces)
+      this.listLoadedPlaces = this.releventPlaces.slice(1);
     });
   }
 
@@ -36,8 +42,11 @@ export class DiscoverPage implements OnInit, OnDestroy {
   //   this.listLoadedPlaces = this.loadedPlaces.slice(1);
   // }
 
-  segmentChanged(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+  segmentChanged(filter: string) {
+    const isShown = place => filter === 'all' || place.userId !== this.authService.userId;
+    this.releventPlaces = this.loadedPlaces.filter(isShown);
+    this.filter = filter;
+    this.listLoadedPlaces = this.releventPlaces.slice(1);
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -10,8 +10,9 @@ import { take, map, tap, delay } from 'rxjs/operators';
 export class PlacesService {
   private _places = new BehaviorSubject<Place[]>(
     [
-      new Place('p1', 'Chicago Apartment', 'Edgewater gem, with a porch', 'https://images1.apartments.com/i2/Mel4Hm5GU_A13jVZId0JNlaZaYgQpTgCahWJI-5laEc/117/elevate-chicago-il-primary-photo.jpg', 950, new Date('2021-01-01'), new Date('2024-12-31'), 'qbc'),
-      new Place('p2', 'Minnesota Apartment', 'St Paul, Duplex', 'https://assets3.thrillist.com/v1/image/1703071/414x310/crop;jpeg_quality=65.jpg', 1100, new Date('2021-01-01'), new Date('2024-12-31'), "123"),
+      new Place('p1', 'Chicago Apartment', 'Edgewater gem, with a porch', 'https://images1.apartments.com/i2/Mel4Hm5GU_A13jVZId0JNlaZaYgQpTgCahWJI-5laEc/117/elevate-chicago-il-primary-photo.jpg', 950, new Date('2021-01-01'), new Date('2024-12-31'), 'xyz'),
+      new Place('p2', 'Minnesota Apartment', 'St Paul, Duplex', 'https://assets3.thrillist.com/v1/image/1703071/414x310/crop;jpeg_quality=65.jpg', 1100, new Date('2021-01-01'), new Date('2024-12-31'), "abc"),
+      new Place('p3', 'Minnesota Apartment', 'St Paul, Duplex', 'https://assets3.thrillist.com/v1/image/1703071/414x310/crop;jpeg_quality=65.jpg', 1100, new Date('2021-01-01'), new Date('2024-12-31'), "xyz"),
     ]
   );
 
